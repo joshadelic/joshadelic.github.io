ea210198165025d746ec770eafd5194fcf0e938a
Joshua Boehringer <joshadelic@gmail.com>
2017-10-25 16:25:15 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,228 @@
+<!doctype html>
+<html lang="en" itemscope itemtype="http://schema.org/Person">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+    <title>Joshua Boehringer</title>
+
+    <!-- fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lobster|Open+Sans|Open+Sans+Condensed:300,700">
+
+    <link rel="stylesheet" href="/css/screen.min.css?0519dae3" media="all">
+
+    <!-- Font Awesome -->
+    <script src="https://use.fontawesome.com/b86593ccc6.js"></script>
+</head>
+
+<body>
+
+    <section class="header">
+        <div class="container">
+            <div class="row row-flex">
+                <div class="col-sm-6 col-md-5 col-lg-4">
+                    <h1>Joshua Boehringer</h1>
+                    <address>
+                        125 Overlook Drive<br>
+                        Holliston, MA 01746<br>
+                        <a href="tel:5083098084" target="_blank">508.309.8084</a><br>
+                        <a href="mailto:joshadelic@gmail.com" target="_blank">joshadelic@gmail.com</a>
+                    </address>
+                </div>
+                <div class="col-sm-6 col-md-7 col-lg-8">
+                    <div class="page-header text-center">
+                        <h3 class="text-uppercase">Front End Web Developer</h3>
+                        <h4>Specializing in real, hand-coded <br>HTML, CSS &amp; JavaScript</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="main">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-4">
+                    <h3>Specialties</h3>
+                    <ul class="list-inline list-separated">
+                        <li>Front End Web Development</li>
+                        <li>HTML</li>
+                        <li>HTML5</li>
+                        <li>PHP</li>
+                        <li>MySQL</li>
+                        <li>CSS</li>
+                        <li>CSS3</li>
+                        <li>SASS</li>
+                        <li>SCSS</li>
+                        <li>Flexbox</li>
+                        <li>JavaScript</li>
+                        <li>ES6</li>
+                        <li>jQuery</li>
+                        <li>Node</li>
+                        <li>Vue.js</li>
+                        <li>Angular</li>
+                        <li>Grunt</li>
+                        <li>Gulp</li>
+                        <li>Babel</li>
+                        <li>AJAX</li>
+                        <li>UI</li>
+                        <li>UX</li>
+                        <li>SEO</li>
+                        <li>Google Tag Manager</li>
+                        <li>Git</li>
+                        <li>GitHub</li>
+                        <li>Bitbucket</li>
+                        <li>Sublime Text</li>
+                        <li>Responsive Design</li>
+                        <li>Web Fonts &amp; Typography</li>
+                        <li>Twitter Bootstrap</li>
+                        <li>Zurb Foundation</li>
+                        <li>Adobe Photoshop</li>
+                        <li>Adobe Illustrator</li>
+                        <li>Adobe Fireworks</li>
+                        <li>Sketch</li>
+                    </ul>
+
+                    <hr>
+
+                    <h3>Education</h3>
+                    <h5>Bunker Hill Community College <small>Charlestown, MA</small></h5>
+                    <p>
+                        AS: Information Technology<br>
+                        Magna cum laude &mdash; GPA: 3.89<br>
+                        <small>(2009-2011)</small>
+                    </p>
+
+                    <h5>Pennsylvania State University <small>University Park, PA</small></h5>
+                    <p>
+                        Liberal Arts<br>
+                        <small>(1997-1998)</small>
+                    </p>
+
+                    <hr>
+
+                    <h3>Awards</h3>
+                    <h5>National Science Foundation</h5>
+                    <p>
+                        Internship at Glasgow Caledonian University<br>
+                        Glasgow, Scotland<br>
+                        <small>(Summer 2011)</small>
+                    </p>
+
+                    <h5>Bunker Hill Community College</h5>
+                    <ul class="list-unstyled">
+                        <li>Dean&rsquo;s List <small>(2010-2012)</small></li>
+                        <li>Academic Excellence Award <small>(2012)</small></li>
+                    </ul>
+
+                </div>
+                <div class="col-sm-7 col-sm-offset-1">
+                    <h3>Recent Work</h3>
+
+                    <h4>
+                        PJA Advertising + Marketing
+                        <small>Cambridge, MA</small>
+                        <small>(04/2016 - present)</small>
+                    </h4>
+                    <h5>Web Developer</h5>
+                    <ul class="list-unstyled">
+                        <li>Develop clean, portable code for a range of agency projects, including responsively designed web sites, campaign landing pages, and experimental in-house projects.</li>
+                        <li>Develop prototypes and proofs of concept.</li>
+                        <li>Keep skills up-to-date in core technologies; research and learn emerging technologies.</li>
+                    </ul>
+                    <p>Projects:
+                        <ul class="list-unstyled">
+                            <li>Working with many national and regional brands to develop second-to-none online experiences (Tracfone, Total Wireless, Straight Talk, Shire, Molecular Health, Cambridge Trust &mdash; to name a few).</li>
+                        </ul>
+                    </p>
+
+                    <hr>
+
+                    <h4>
+                        Weymouth Design
+                        <small>Boston, MA</small>
+                        <small>(10/2015 - 04/2016)</small>
+                    </h4>
+                    <h5>Senior Front End Developer</h5>
+                    <ul class="list-unstyled">
+                        <li>Working with a small team at a boutique full-service agency in Boston &mdash; creating cutting-edge web solutions for a variety of clients.</li>
+                    </ul>
+                    <p>Projects:
+                        <ul class="list-unstyled">
+                            <li>MC10, Inc.: <a href="https://mc10inc.com/" target="_blank" rel="noopener noreferrer">https://mc10inc.com/</a></li>
+                            <li>Concord Academy: <a href="https://concordacademy.org/" target="_blank" rel="noopener noreferrer">https://concordacademy.org/</a></li>
+                        </ul>
+                    </p>
+
+                    <hr>
+
+                    <h4>
+                        Barbershop Labs
+                        <small>Framingham, MA</small>
+                        <small>(11/2013 - 04/2016)</small>
+                    </h4>
+                    <h5>Freelance Front End Developer</h5>
+                    <ul>
+                        <li>Partnering with this Boston-based Ruby on Rails and mobile development shop for front end freelance work.</li>
+                        <li>Working with a small team of technology veterans, leveraging modern technologies and proven methodologies to build world-class web and mobile apps.</li>
+                    </ul>
+                    <p>Projects:
+                        <ul class="list-unstyled">
+                            <li>The Purple Carrot: <a href="https://www.thepurplecarrot.com/" target="_blank" rel="noopener noreferrer">https://www.thepurplecarrot.com/</a></li>
+                            <li>Renoviso: <a href="https://renoviso.com/" target="_blank" rel="noopener noreferrer">https://renoviso.com/</a></li>
+                            <li>Barbershop Labs website:<a href="https://barbershoplabs.com/" target="_blank" rel="noopener noreferrer">https://barbershoplabs.com/</a></li>
+                        </ul>
+                    </p>
+
+
+                    <hr>
+
+                    <h4>
+                        HealthTalker
+                        <small>Newton, MA</small>
+                        <small>(10/2012 - 03/2015)</small>
+                    </h4>
+                    <h5>Front End Developer</h5>
+                    <ul>
+                        <li>Hand coding high quality HTML, CSS, and JavaScript in a Ruby on Rails environment.</li>
+                        <li>Working closely with designers to bring their designs to life, and with developers to merge the front end with the back end.</li>
+                        <li>Working with the team to improve the user experience for HealthTalker’s offerings.</li>
+                        <li>Participating in all phases of the agile development cycle and meticulously iterating the front end based on feedback received.</li>
+                    </ul>
+                    <p>Projects:
+                        <ul class="list-unstyled">
+                            <li>All projects are not publicly accessible, but I can provide code samples.</li>
+                        </ul>
+                    </p>
+
+                    <hr>
+
+                    <h4>
+                        Verndale
+                        <small>Boston, MA</small>
+                        <small>(04/2012 - 09/2012)</small>
+                    </h4>
+                    <h5>Front End Developer</h5>
+                    <ul>
+                        <li>Develop advanced and flexible UI templates based on prototypes, design comps and interaction documents.</li>
+                        <li>Develop clean, reusable, and well documented front-end code using advanced JavaScript, jQuery, HTML5 &amp; CSS3 techniques, with a focus on object-oriented design.</li>
+                        <li>Produce elegant interactive solutions to complex problems that perform effectively across cross-browser and cross-domain environments.</li>
+                        <li>Work hand-in-hand with engineering, graphic design, user experience and project management teams to analyze requirements and outline proposed solutions.</li>
+                    </ul>
+                    <p>Projects:
+                        <ul class="list-unstyled">
+                            <li>Business Software Alliance: <a href="http://www.bsa.org/" target="_blank" rel="noopener noreferrer">http://www.bsa.org/</a></li>
+                            <li>Marble &amp; Granite: <a href="http://www.marbleandgranite.com/" target="_blank" rel="noopener noreferrer">http://www.marbleandgranite.com/</a></li>
+                            <li>Hollingsworth &amp; Vose: <a href="http://www.hollingsworth-vose.com/" target="_blank" rel="noopener noreferrer">http://www.hollingsworth-vose.com/</a></li>
+                        </ul>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <script src="/js/app.min.js?e5f930bb"></script>
+</body>
+</html>
